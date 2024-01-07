64e32349274032231eca10f3035f1f0bdf2a910d
lisahalman <61053821+lisahalman@users.noreply.github.com>
2024-01-07 17:13:20 +0100

    day3 pt 1 complete and part 2

--- src/Day3Part2.java
+++ src/Day3Part2.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.*;
 
-public class Day3 {
+public class Day3Part2 {
 
     public static Set<String> setOfCoordinatesOfStartOfNumberWhichIsAdded = new LinkedHashSet<>();
 
@@ -23,7 +23,7 @@ public class Day3 {
         for (int i = 0; i < lengthOfALine; i++) {
             for (int j = 0; j < howManyLines; j++) {
                 if (!(engine[i][j] == '.' || (engine[i][j] >= '0' && engine[i][j] <= '9'))) {
-                    int number = checkEveryThingAround(engine, i, j);
+                    long number = checkEveryThingAround(engine, i, j);
                     sumOfNumbers += number;
                 }
             }
@@ -31,43 +31,72 @@ public class Day3 {
         System.out.println("Sum: " + sumOfNumbers);
     }
 
-    public static int checkEveryThingAround(char[][] engine, int i, int j) {
-        int totalOfNumbersAround = 0;
+    public static long checkEveryThingAround(char[][] engine, int i, int j) {
+        ArrayList<Integer> listOfTotalNumbersAround = new ArrayList<>();
         if (i - 1 >= 0 && j - 1 >= 0) {
             if (engine[i - 1][j - 1] >= '0' && engine[i - 1][j - 1] <= '9') {
                 char newNumber = engine[i - 1][j - 1];
-                totalOfNumbersAround += getWholeNumber(engine, i - 1, j - 1, ("" + newNumber));
+                int wholeNumber = getWholeNumber(engine, i - 1, j - 1, ("" + newNumber));
+                if (wholeNumber != 0 ) {
+                    listOfTotalNumbersAround.add(wholeNumber);
+                }
             }
             if (engine[i - 1][j] >= '0' && engine[i - 1][j] <= '9') {
                 char newNumber = engine[i - 1][j];
-                totalOfNumbersAround += getWholeNumber(engine, i - 1, j, ("" + newNumber));
+                int wholeNumber = getWholeNumber(engine, i - 1, j, ("" + newNumber));
+                if (wholeNumber != 0 ) {
+                    listOfTotalNumbersAround.add(wholeNumber);
+                }
             }
             if (engine[i - 1][j + 1] >= '0' && engine[i - 1][j + 1] <= '9') {
                 char newNumber = engine[i - 1][j + 1];
-                totalOfNumbersAround += getWholeNumber(engine, i - 1, j + 1, ("" + newNumber));
+                int wholeNumber = getWholeNumber(engine, i - 1, j + 1, ("" + newNumber));
+                if (wholeNumber != 0 ) {
+                    listOfTotalNumbersAround.add(wholeNumber);
+                }
             }
             if (engine[i][j - 1] >= '0' && engine[i][j - 1] <= '9') {
                 char newNumber = engine[i][j - 1];
-                totalOfNumbersAround += getWholeNumber(engine, i, j - 1, ("" + newNumber));
+                int wholeNumber = getWholeNumber(engine, i, j - 1, ("" + newNumber));
+                if (wholeNumber != 0 ) {
+                    listOfTotalNumbersAround.add(wholeNumber);
+                }
             }
             if (engine[i][j + 1] >= '0' && engine[i][j + 1] <= '9') {
                 char newNumber = engine[i][j + 1];
-                totalOfNumbersAround += getWholeNumber(engine, i, j + 1, ("" + newNumber));
+                int wholeNumber = getWholeNumber(engine, i, j + 1, ("" + newNumber));
+                if (wholeNumber != 0 ) {
+                    listOfTotalNumbersAround.add(wholeNumber);
+                }
             }
             if (engine[i + 1][j - 1] >= '0' && engine[i + 1][j - 1] <= '9') {
                 char newNumber = engine[i + 1][j - 1];
-                totalOfNumbersAround += getWholeNumber(engine, i + 1, j - 1, ("" + newNumber));
+                int wholeNumber = getWholeNumber(engine, i + 1, j - 1, ("" + newNumber));
+                if (wholeNumber != 0 ) {
+                    listOfTotalNumbersAround.add(wholeNumber);
+                }
             }
             if (engine[i + 1][j] >= '0' && engine[i + 1][j] <= '9') {
                 char newNumber = engine[i + 1][j];
-                totalOfNumbersAround += getWholeNumber(engine, i + 1, j, ("" + newNumber));
+                int wholeNumber = getWholeNumber(engine, i + 1, j, ("" + newNumber));
+                if (wholeNumber != 0 ) {
+                    listOfTotalNumbersAround.add(wholeNumber);
+                }
             }
             if (engine[i + 1][j + 1] >= '0' && engine[i + 1][j + 1] <= '9') {
                 char newNumber = engine[i + 1][j + 1];
-                totalOfNumbersAround += getWholeNumber(engine, i + 1, j + 1, ("" + newNumber));
+                int wholeNumber = getWholeNumber(engine, i + 1, j + 1, ("" + newNumber));
+                if (wholeNumber != 0 ) {
+                    listOfTotalNumbersAround.add(wholeNumber);
+                }
             }
         }
-        return totalOfNumbersAround;
+        if (listOfTotalNumbersAround.size() == 2) {
+            long total;
+            total = (long) listOfTotalNumbersAround.get(0) * listOfTotalNumbersAround.get(1);
+            return total;
+        }
+        return 0;
     }
 
     public static int getWholeNumber(char[][] engine, int i, int j, String numberToAdd) {
